c5908ddb9d40ae7ac11b77bc5b95378b8761c514
Zach McMurray <zmcmurray14@gmail.com>
2022-11-27 23:16:46 -0500

    code should work I think

--- MiniProject2.c
+++ MiniProject2.c
@@ -6,14 +6,14 @@ struct user_t
     double latitude;
     double longtitude;
     double altitude;
-    double time;
+    int time;
     char name[20];
 };
 
 
 void fileReader(FILE *readFile, struct user_t user_o[], struct user_t* use_1){
     int x; fscanf(readFile,"%i", &x);
-    char name[10];
+    char name[20];
     int time;
     double lng, lat, alt;
     fscanf(readFile,"%s", &name);
@@ -42,11 +42,37 @@ void fileReader(FILE *readFile, struct user_t user_o[], struct user_t* use_1){
 
 }
 
+void otherUsers(struct user_t user_o[],int size){
+    for(int x=0;x<size;x++){
+    printf("enter %i user lat: ", x+1);
+    scanf("%lf",&user_o[x].latitude);
+    printf("enter %i user long: ", x+1);
+    scanf("%lf",&user_o[x].longtitude);
+    printf("enter %i user alt: ", x+1);
+    scanf("%lf",&user_o[x].altitude);
+    printf("enter %i user time: ", x+1);
+    scanf("%i",&user_o[x].time);
+    printf("enter %i user name: ", x+1);
+    scanf("%s",&user_o[x].name);
+    }
+}
+
 void difference(struct user_t user_o[], struct user_t use_1, double array[],int size){
     for(int y=0;y<size;y++){
         array[y]=sqrt(((use_1.altitude-user_o[y].altitude)*(use_1.altitude-user_o[y].altitude))+((use_1.latitude-user_o[y].latitude)*(use_1.latitude-user_o[y].latitude))+((use_1.longtitude-user_o[y].longtitude)*(use_1.longtitude-user_o[y].longtitude)));
     }
-    printf("%lf %lf %lf", array[0], array[1], array[2]);
+}
+
+void nearUser(struct user_t user_o[], struct user_t use_1, double array[],int size){
+double small = array[0];
+int countSave=0;
+for(int x=1;x<size;x++){
+    if(array[x]<small){
+        small = array[x];
+        countSave = x;
+    }
+}
+printf("%s is closest to %s at %.2lf",user_o[countSave].name,use_1.name,array[countSave]);
 }
 
 int main(void){
@@ -76,14 +102,19 @@ int main(void){
         double arrdis[size];
         fileReader(fopen(fileName, "r"),arr,&our_user);
         difference(arr,our_user,arrdis,size);
+        nearUser(arr,our_user,arrdis,size);
     }
     else if (txt == 'n' || txt == 'N')
     {
         printf("What is your users data? (lat long alt time name)");
-        scanf("%ld %ld %ld %ld %s", &our_user.latitude, &our_user.longtitude, &our_user.altitude, &our_user.time, &our_user.name);
+        scanf("%lf %lf %lf %i %s", &our_user.latitude, &our_user.longtitude, &our_user.altitude, &our_user.time, &our_user.name);
         printf("How many other users are you inputting?");
         scanf(" %d", &othernum);
         struct user_t other_users[othernum];
+        double arrdis[othernum];
+        otherUsers(other_users,othernum);
+         difference(other_users,our_user,arrdis,othernum);
+         nearUser(other_users,our_user,arrdis,othernum);
     }
     
     
